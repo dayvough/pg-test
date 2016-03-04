42e88645966628867d1610f389da6e81b6c86f05
David Marquez <david.marquez@me.com>
2016-03-04 20:55:39 +0800

    Still nailing general unit test for User. Haven't started on multi-version records

--- models/user.rb
+++ models/user.rb
@@ -1,11 +1,37 @@
+
+# User attributes:
+# -- user_id    : Integer
+# -- first_name : String
+# -- last_name  : String
+# -- email      : String
+# -- password   : String
+# -- api_key    : String
+# -- created_at : DateTime
+# -- updated_at : DateTime
+# -- version    : Integer
+
+# Email Validator from ActiveRecord docs, called on "email: true"
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :email, :password
-  after_initialize :init, unless: :persisted?
+  validates :email, email: true, uniqueness: true, on: :create
+  after_save :init
 
   def init
-    self.user_id    = self.id
-    self.version    = 1
+    self.user_id = self.id
+    self.version = 1
+    self.api_key = self.last_name + self.first_name
   end
 
-  
+  # Grabs the current user, creates a duplicate,
+  # and increments the duplicated version
+  # def update
+  # end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -16,14 +16,50 @@ describe 'Users' do
     expect(User.count).to eq(0)
   end
 
-  it "should be able to add a User with proper info" do
-    User.create(first_name: 'A', last_name: 'B', email: 'C', password: 'D')
+  it "should be able to create a User with proper info" do
+    user = User.create(first_name: 'A', last_name: 'B', email: 'C@example.com', password: 'D')
     expect(User.count).to eq(1)
+    expect(user.id).to eq(1)
   end
 
   it "shouldn't add a User with missing info" do
     User.create(first_name: 'ABC')
     expect(User.count).to eq(0)
+    User.create(last_name: 'ABC')
+    expect(User.count).to eq(0)
+    User.create(email: 'ABC')
+    expect(User.count).to eq(0)
+    User.create(password: 'ABC')
+    expect(User.count).to eq(0)
+  end
+
+  it "should have all the meta data" do
+    expect(User.count).to eq(0)
+    user = User.create(first_name: 'E', last_name: 'F', email: 'G@shirt.ly', password: 'H')
+    expect(User.count).to eq(1)
+    expect(user.id).to eq(2)
+    expect(user.user_id).to eq(user.id)
+    expect(user.first_name).to eq('E')
+    expect(user.last_name).to eq('F')
+    expect(user.email).to eq('G@shirt.ly')
+    expect(user.password).to eq('H')
+    expect(user.api_key).to eq(user.last_name + user.first_name)
+    expect(user.version).to eq(1)
+  end
+
+  # Checks if user count equals 3 and ID's progress naturally
+  # DB doesn't clean on case so the user ID's keep incrementing
+  it "should have the correct ID" do
+    expect(User.count).to eq(0)
+    user = User.create(first_name: 'E', last_name: 'F', email: 'G@dev.io', password: 'H')
+    expect(User.count).to eq(1)
+    expect(user.id).to eq(3)
+  end
+
+  # RegEx from ActiveRecord docs
+  it "should have a valid email" do
+    user = User.create(first_name: 'I', last_name: 'J', email: 'K@test.me', password: 'L')
+    expect(user.email).to match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i)
   end
 
 end
